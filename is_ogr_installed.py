c71c91b4f4981135284fec9137b3f64d45830702
Marjorie Roswell <mroswell@gmail.com>
2014-01-22 15:30:33 -0500

    Create is_ogr_installed.py
    from http://pcjericks.github.io/py-gdalogr-cookbook/layers.html

---
+++ is_ogr_installed.py
@@ -0,0 +1,5 @@
+try:                                                                                                                                                                                                                                  
+  from osgeo import ogr                                                                                                                                                                                                               
+  print 'Import of ogr from osgeo worked.  Hurray!\n'                                                                                                                                                                                 
+except:                                                                                                                                                                                                                               
+  print 'Import of ogr from osgeo failed\n\n'     
