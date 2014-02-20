b7374b01ca5018ed49a94fb9ae45728fc2acf960
Marjorie Roswell <mroswell@gmail.com>
2014-02-20 01:41:52 -0500

    generate web mercator projection shapefiles
    These are in the 'webm' directory, with subdirectories by state

--- download_transform_census_shapefiles_by_state.py
+++ download_transform_census_shapefiles_by_state.py
@@ -12,18 +12,19 @@ TIGER_SHAPE_TYPES = {
     'upper': 'sldu',
     'lower': 'sldl',
 }
-TIGER_SHAPE_TYPES = {'congress': 'cd113'} # remove this when not testing
+#TIGER_SHAPE_TYPES = {'congress': 'cd113'} # remove this when not testing
 TIGER_DISTRICTS_URL_BASE = 'http://www2.census.gov/geo/tiger/TIGERrd13_st'
 OGR2OGR = '/Library/Frameworks/GDAL.framework/Versions/Current/Programs/ogr2ogr'
 OGRINFO = '/Library/Frameworks/GDAL.framework/Versions/Current/Programs/ogrinfo'
 
 # mkOGR_NAD84_2_webm_cmd = lambda src, dst: (OGR2OGR, '-f', 'ESRI Shapefile', dst, src, '-s_srs', 'EPSG:4269', '-t_srs', 'EPSG:3857' )
-mkOGR_NAD84_2_webm_cmd = lambda src, dst: (OGR2OGR + ' -f ESRI Shapefile '+dst +' '+ src + ' s_srs EPSG:4269 -t_srs EPSG:3857 -overwrite' )
+mkOGR_NAD84_2_webm_cmd = lambda src, dst: (OGR2OGR + ' -f "ESRI Shapefile" '+dst +' '+ src + ' -s_srs "EPSG:4269" -t_srs "EPSG:3857" -overwrite' )
 info_cmd = lambda src: (OGRINFO+' -so '+ src)
 DOWNLOAD_DIR = os.path.expanduser('~/Desktop/ogr2ogr-tests/downloads'); makedirs(DOWNLOAD_DIR)
 WEBM_DIR = os.path.expanduser('~/Desktop/ogr2ogr-tests/webm'); makedirs(WEBM_DIR)
+print("WEBM_DIR", WEBM_DIR)
 TMP_DIR = os.path.expanduser('~/Desktop/ogr2ogr-tests/tmp'); makedirs(TMP_DIR)
-
+#transform_file = open('transform.sh', "wt")
 
 # The full fips.csv file is here (below the python script)
 # http://vvanhee.wordpress.com/2012/01/25/download-all-u-s-census-block-shapefiles/
@@ -34,15 +35,20 @@ for row in fips_csv:
 #    statename = row['statename'].upper().replace(' ','_')
     statefips = row['code'].zfill(2)
 
-    print repr(mkOGR_NAD84_2_webm_cmd('abcd', 'barlksjdf'))
+ #   print repr(mkOGR_NAD84_2_webm_cmd('abcd', 'barlksjdf'))
 
     statedownloads = os.path.join(DOWNLOAD_DIR, stateabbrev)
     makedirs(statedownloads)
     os.chdir(statedownloads)
     webmaps = os.path.join(WEBM_DIR, stateabbrev)
+    makedirs(webmaps)
 
     for stype in TIGER_SHAPE_TYPES.itervalues():
         filename = 'tl_rd13_%(statefips)s_%(stype)s' % {'statefips': statefips, 'stype': stype, }
+        fullfilename = (os.path.join(DOWNLOAD_DIR, stateabbrev, filename))
+        webmapname = (os.path.join(webmaps, filename))
+
+        print(fullfilename+".shp")
         url = '%(base)s/%(statefips)s/%(filename)s.zip' % { 'base': TIGER_DISTRICTS_URL_BASE, 'statefips': statefips, 'filename': filename, }
         print(url)
         print 'getting %(url)s (%(stateabbrev)s)' % {'url': url, 'stateabbrev': stateabbrev, }
@@ -53,7 +59,14 @@ for row in fips_csv:
 
             os.system('unzip ' + filename + ".zip")
 
-            os.system(repr(info_cmd(filename+".shp")))
+#            strinfocmd = repr(info_cmd(fullfilename+".shp"))
+#            os.system(strinfocmd)
+            strtransformcmd = repr(mkOGR_NAD84_2_webm_cmd(fullfilename +".shp", webmapname+".shp"))
+            print("\n---")
+            print(strtransformcmd)
+            print ("---")
+#            os.system(strtransformcmd)
+            os.system(OGR2OGR + ' -f "ESRI Shapefile" '+webmapname +'.shp '+ filename + '.shp -s_srs "EPSG:4269" -t_srs "EPSG:3857" -overwrite' )
 #            os.system(repr(mkOGR_NAD84_2_webm_cmd(filename +".shp", filename+"webm.shp")) )
           # os.system ('/Library/Frameworks/GDAL.framework/Versions/Current/Programs/ogr2ogr -f ESRI Shapefile ' + filename+"webm.shp " + filename + ".shp  -s_srs EPSG:4269 -t_srs EPSG:3857" )
 
